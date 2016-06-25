d0cc7e3b3a53d7974bd524658c21d64951eca03d
lijinzhao <leecynical@gmail.com>
2016-06-25 15:16:49 +0800

    01- 配置信息

--- 百思不得姐/AppDelegate.m
+++ 百思不得姐/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,8 +16,13 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+    self.window = [[UIWindow alloc] init];
+    self.window.frame = [[UIScreen mainScreen] bounds];
+    
+    self.window.rootViewController = [[ViewController alloc] init];
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
