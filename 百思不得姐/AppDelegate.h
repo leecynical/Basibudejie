d16815956bd64f0dc0376d6a048095b7ae5bed59
lijinzhao <leecynical@gmail.com>
2016-06-25 14:41:06 +0800

    Initial Commit

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 李金钊 on 16/6/25.
+//  Copyright © 2016年 lijinzhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
